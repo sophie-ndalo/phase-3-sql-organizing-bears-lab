9241caf50ce4587c43a6efccda0c5ca17e5ccb13
sophia <sophiendalo@gmail.com>
2023-02-17 03:46:54 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears(name,age, sex, color,temperament,alive) 
+VALUES('Mr.Chocolate',7,'M','red','happy',true);
+
+INSERT INTO bears(name,age, sex, color,temperament,alive) 
+VALUES('Rowdy',10,'M','red','happy',true);
+
+INSERT INTO bears(name,age, sex, color,temperament,alive)
+VALUES('Tabitha',6,'F','red','happy',true);
+
+INSERT INTO bears(name,age, sex, color,temperament,alive) 
+VALUES('Sergeant Brown',7,'M','red','happy',true);
+
+INSERT INTO bears(name,age, sex, color,temperament,alive) 
+VALUES('Melissa',13,'F','red','happy',true);
+
+INSERT INTO bears(name,age, sex, color,temperament,alive) 
+VALUES('Grinch',2,'F','red','happy',true);
+
+INSERT INTO bears(name,age, sex, color,temperament,alive) 
+VALUES('Wendy',6,'F','red','happy',true);
+
+INSERT INTO bears(name,age, sex, color,temperament,alive) 
+VALUES(NULL,7,'M','red','happy',true);
